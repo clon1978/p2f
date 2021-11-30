8a08c69bfd9b63e2ad70627eb3e093166facc510
clon1978 <30918640+clon1978@users.noreply.github.com>
2021-11-29 21:36:05 -0800

    Update credentials.py

--- source/credentials.py
+++ source/credentials.py
@@ -2,7 +2,7 @@ class AlpacaConfig:
     # Alpaca key:
     API_KEY = "PKHDG1PMJU0259X83GH7"
     # Alpaca secret:
-    API_SECRET = "cf9wcbkLPaEub8W3n3b639SOx7mGxwybNzr7LBLA"
+    API_SECRET = ""
     # For paper trading
     ENDPOINT = "https://paper-api.alpaca.markets"
 
